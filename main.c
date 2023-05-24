030e5d446b99849d1f2dc117790e74644390a5d1
Rida Rhnizar <rrhnizar@c2r9p4.1337.ma>
2023-05-24 23:06:32 +0100

    last check

--- main.c
+++ main.c
@@ -1,31 +1,73 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * sig_handl - Ctrl + C
- * @sig: signal
+ * free_data - frees data structure
+ *
+ * @datash: data structure
+ * Return: no return
  */
-
-void sig_handl(int sig)
+void free_data(data_shell *datash)
 {
-	if (sig == SIGINT)
-		write(1, "\nsimple_shell ~ ", 16);
+	unsigned int i;
+
+	for (i = 0; datash->_environ[i]; i++)
+	{
+		free(datash->_environ[i]);
+	}
+
+	free(datash->_environ);
+	free(datash->pid);
 }
 
 /**
- * main - main programme
- * @argc : arguments count
- * @argv : arguments values
- * @env : environment
- * Return: int
+ * set_data - Initialize data structure
+ *
+ * @datash: data structure
+ * @av: argument vector
+ * Return: no return
  */
-
-int main(int argc, char **argv, char **env)
+void set_data(data_shell *datash, char **av)
 {
-	char		*path;
+	unsigned int i;
 
-	(void)argc;
-	(void)argv;
-	signal(SIGINT, sig_handl);
-	path = getenv("PATH");
-	return (0);
+	datash->av = av;
+	datash->input = NULL;
+	datash->args = NULL;
+	datash->status = 0;
+	datash->counter = 1;
+
+	for (i = 0; environ[i]; i++)
+		;
+
+	datash->_environ = malloc(sizeof(char *) * (i + 1));
+
+	for (i = 0; environ[i]; i++)
+	{
+		datash->_environ[i] = _strdup(environ[i]);
+	}
+
+	datash->_environ[i] = NULL;
+	datash->pid = aux_itoa(getpid());
+}
+
+/**
+ * main - Entry point
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	data_shell datash;
+	(void) ac;
+
+	signal(SIGINT, get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
 }
